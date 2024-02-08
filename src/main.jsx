1ed005c3208c1b019cf1742d0f277eb21daa4f9d
Indrarjun <billasaketh@gmail.com>
2024-02-08 19:46:07 +0530

    Added disabling react devtools for production

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,9 @@ import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
 
+if (process.env.NODE_ENV === "production") disableReactDevTools();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
