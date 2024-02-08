d007ba753729eab44436381a1e97707080ade453
Indrarjun <billasaketh@gmail.com>
2024-02-08 19:48:43 +0530

    updated

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,8 @@ import { BrowserRouter } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { disableReactDevTools } from "@fvilers/disable-react-devtools";
-
+import dotenv from "dotenv";
+dotenv.config();
 if (process.env.NODE_ENV === "production") disableReactDevTools();
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
